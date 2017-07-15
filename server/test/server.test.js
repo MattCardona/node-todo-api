d60d761318c0895fe9b4708ff24e8755d5ff4710
Matt <boblikesbbq@gmail.com>
2017-07-15 13:23:40 -0700

    Add tests for POST /users/login

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -218,7 +218,7 @@ describe('Post /users', () => {
         expect(user).toExist();
         expect(user.password).toNotBe(password);
         done();
-      })
+      }).catch((err) => done(err));
     });
   });
 
@@ -244,4 +244,55 @@ describe('Post /users', () => {
     .end(done);
   })
 
+});
+
+describe('POST /users/login', () => {
+  it('should login user and auth token', (done) => {
+    request(app)
+    .post('/users/login')
+    .send({
+      email: users[1].email,
+      password: users[1].password
+    })
+    .expect(200)
+    .expect((res) => {
+      expect(res.headers['x-auth']).toExist();
+    })
+    .end((err, res) => {
+      if(err){
+        return done(err);
+      }
+
+      User.findById(users[1]._id).then((doc) => {
+        expect(doc.tokens[0]).toInclude({
+          access: 'auth',
+          token: res.headers['x-auth']
+        });
+        done();
+      }).catch((err) => done(err));
+    });
+  });
+
+  it('should reject invalid login', (done) => {
+    request(app)
+    .post('/users/login')
+    .send({
+      email: users[1].email,
+      password: 'wrongpassword'
+    })
+    .expect(400)
+    .expect((res) => {
+      expect(res.headers['x-auth']).toNotExist();
+    })
+    .end((err, res) => {
+      if(err){
+        return done(err);
+      }
+
+      User.findById(users[1]._id).then((doc) => {
+        expect(doc.tokens.length).toBe(0);
+        done();
+      }).catch((err) => done(err));
+    });
+  });
 });
